9268a08565cfabd0567815acf6313dbe960519e6
Marvin Clerge <clergemarvin@gmail.com>
2018-10-10 12:01:06 -0400

    session controller routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,8 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  # SessionsController routes
+  post "/login", to: "sessions#login";
+  post "/signup", to: "sessions#signup";
+  get "/current", to: "sessions#get_current_user";
 end
